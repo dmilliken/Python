88d6c40d601b546a8fa411fd690d31fcd29ec8cf
Darcie Milliken <darciemilliken@gmail.com>
2015-03-18 09:14:41 -0600

    Guessing game example updated to 3.4

--- exercises/Guessing_Game.py
+++ exercises/Guessing_Game.py
@@ -1,16 +1,16 @@
 from random import randint
 x = randint(1,10)
-print "I'm thinking of a number between 1 and 10"
+print("I'm thinking of a number between 1 and 10")
 
 correct = False
 
 while not correct:
-    guess = raw_input("Guess the number\n")
+    guess = input("Guess the number\n")
     guess = int(guess)
     if guess > x:
-        print 'Too high'
+        print("Too high")
     elif guess < x:
-        print 'Too low'
+        print("Too low")
     else:
-        print "You're right!"
+        print("You're right!")
         correct = True
