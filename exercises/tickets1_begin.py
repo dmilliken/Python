82106e3ee641fdee30b21dcce57373b9918bedcd
Darcie Milliken <darciemilliken@gmail.com>
2015-03-18 09:45:27 -0600

    Updated ticket examples

--- exercises/tickets1_begin.py
+++ exercises/tickets1_begin.py
@@ -2,6 +2,6 @@ num_tickets = 8
 
 tickets_requested = # ask the user how many tickets she wants
 
-print "You've asked for {} ticket(s)".format(tickets_requested)
+print("You've asked for {} ticket(s)".format(tickets_requested))
 # deduct the tickets sold from available tickets
 print #the number of remaining tickets
